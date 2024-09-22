0a06f10fa4562a3c65e8faa8f3693c981db699a8
Chris Maniewski <code@chmanie.com>
2024-09-22 16:16:17 +0200

    feat(leds): add led state machine for simple effects

--- src/tasks/leds.rs
+++ src/tasks/leds.rs
@@ -1,5 +1,3 @@
-use core::array;
-
 use defmt::*;
 use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 use embassy_executor::Spawner;
@@ -8,19 +6,33 @@ use embassy_rp::i2c::{Async, I2c};
 use embassy_rp::peripherals::I2C1;
 use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
 use embassy_sync::channel::Channel;
+use embassy_sync::mutex::Mutex;
 use embassy_time::Timer;
 use is31fl3218::Is31Fl3218;
-use portable_atomic::{AtomicU32, Ordering};
 use {defmt_rtt as _, panic_probe as _};
 
 // FIXME: DOUBLE CHECK ALL CHANNELS and maybe use ATOMICS if possible as we have 16 apps
 // that can potentially lock up a channel
+// FIXME: Add snappy fade out for all LEDs when turning off
 
+const REFRESH_RATE: u64 = 60;
+
+#[derive(Clone, Copy)]
 pub enum LedsAction {
-    Blink(usize),
+    Idle,
+    Blink(u64),
 }
 
-pub static CHANNEL_LEDS: Channel<CriticalSectionRawMutex, LedsAction, 16> = Channel::new();
+impl LedsAction {
+    fn get_value(&self) -> u8 {
+        match self {
+            Self::Blink(_) => 255,
+            Self::Idle => 0,
+        }
+    }
+}
+
+pub static CHANNEL_LEDS: Channel<CriticalSectionRawMutex, (usize, LedsAction), 16> = Channel::new();
 
 pub async fn start_leds(
     spawner: &Spawner,
@@ -33,63 +45,53 @@ pub async fn start_leds(
 async fn run_leds(i2c_device: I2cDevice<'static, NoopRawMutex, I2c<'static, I2C1, Async>>) {
     let mut led_driver = Is31Fl3218::new(i2c_device);
 
+    let refresh_millis = 1000 / REFRESH_RATE;
+
     led_driver.enable_device().await.unwrap();
     led_driver.enable_all().await.unwrap();
-    led_driver.set_all(&[255; 18]).await.unwrap();
 
-    // FIXME: Create array here that holds the values for all LEDs
-    // Then two async loops, one in which we loop over channel messages which set the led values
-    // And one that regularly updates the LED values
-    // Maybe we can have a "changed" value again to not do unnecessary updates
+    let led_state: Mutex<NoopRawMutex, [LedsAction; 18]> = Mutex::new([LedsAction::Idle; 18]);
+    // FIXME: we need a prev_state to be able to override a running effect/action and then go back
+    // to the previous one instead of idle
 
-    // let mut midi_config = Config::default();
-    // midi_config.baudrate = 31250;
-    //
-    // let mut tx_thru = UartTx::new(uart0, pin_tx_thru, dma2, midi_config);
-    //
-    // let uart = Uart::new(uart1, pin_tx, pin_rx, Irqs, dma3, dma4, midi_config);
-    //
-    // let (mut tx, mut rx) = uart.split();
-    //
-    // // unwrap!(spawner.spawn(reader(uart_rx)));
-    //
-    // let uart_tx = async {
-    //     let mut buf = [0; 3];
-    //     loop {
-    //         if let UartAction::SendMidiMsg(msg) = CHANNEL_UART_TX.receive().await {
-    //             if msg.copy_to_slice(&mut buf[..msg.bytes_size()]).is_ok() {
-    //                 tx.write(&buf[..msg.bytes_size()]).await.unwrap();
-    //             }
-    //         }
-    //     }
-    // };
-    //
-    // let uart_tx_thru = async {
-    //     let mut buf = [0; 3];
-    //     loop {
-    //         if let UartAction::SendMidiMsg(msg) = CHANNEL_UART_TX_THRU.receive().await {
-    //             if msg.copy_to_slice(&mut buf[..msg.bytes_size()]).is_ok() {
-    //                 tx_thru.write(&buf[..msg.bytes_size()]).await.unwrap();
-    //             }
-    //         }
-    //     }
-    // };
-    //
-    // let uart_rx = async {
-    //     loop {
-    //         let mut buf = [0; 3];
-    //         rx.read(&mut buf).await.unwrap();
-    //
-    //         match MidiMessage::from_bytes(&buf) {
-    //             Ok(_midi_msg) => {
-    //                 info!("DO SOMETHING WITH THIS MESSAGE: {:?}", buf);
-    //             }
-    //             Err(_err) => {
-    //                 info!("There was an error but we should not panic. Data: {}", buf);
-    //             }
-    //         }
-    //     }
-    // };
-    //
-    // join3(uart_tx, uart_tx_thru, uart_rx).await;
+    let display_fut = async {
+        let mut led_buf: [u8; 18] = [0; 18];
+        let mut changed = false;
+        loop {
+            let mut state = led_state.lock().await;
+            for (i, led) in state.iter_mut().enumerate() {
+                match &led {
+                    // FIXME: this could maybe be in the LedsAction implementation?
+                    LedsAction::Blink(duration) => {
+                        let next_duration = duration.saturating_sub(refresh_millis);
+                        *led = if next_duration == 0 {
+                            LedsAction::Idle
+                        } else {
+                            LedsAction::Blink(next_duration)
+                        };
+                        changed = true;
+                    }
+                    LedsAction::Idle => {}
+                }
+                led_buf[i] = led.get_value();
+            }
+            drop(state);
+            if changed {
+                led_driver.set_all(&led_buf).await.unwrap();
+                changed = false;
+            }
+            // Roughly 60Hz refresh rate
+            Timer::after_millis(refresh_millis).await;
+        }
+    };
+
+    let mod_fut = async {
+        loop {
+            let (chan, action) = CHANNEL_LEDS.receive().await;
+            let mut state = led_state.lock().await;
+            state[chan] = action;
+        }
+    };
+
+    join(display_fut, mod_fut).await;
 }
