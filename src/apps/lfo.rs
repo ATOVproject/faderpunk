3391a6f159ed4a8f42bc719a7db69a5e78e4ebbd
Chris Maniewski <code@chmanie.com>
2025-03-17 21:53:57 +0100

    feat: refactor LFO app

--- src/apps/lfo.rs
+++ src/apps/lfo.rs
@@ -1,60 +1,39 @@
-use defmt::info;
-use embassy_futures::join::{join3, join4};
+use embassy_futures::join::join3;
 
-use crate::app::{App, Global, Range};
-use crate::constants::{CURVE_LOG, WAVEFORM_RECT, WAVEFORM_SAW, WAVEFORM_SINE, WAVEFORM_TRIANGLE};
-
-// API ideas:
-// - app.wait_for_midi_on_channel
+use crate::app::{App, Range};
+use crate::constants::{Waveform, CURVE_LOG};
 
 pub const CHANNELS: usize = 1;
 
 pub async fn run(app: App<CHANNELS>) {
-    info!("App simple LFO started on channel: {}", app.channels[0]);
-
-    let glob_wave: Global<u16> = app.make_global(0);
+    let glob_wave = app.make_global(Waveform::Sine);
     let glob_lfo_speed = app.make_global(0.0682);
-    let glob_lfo_pos = app.make_global(0);
+    let glob_lfo_pos = app.make_global(0.0);
 
     let output = app.make_out_jack(0, Range::_0_10V).await;
-
-    let mut vals: f32 = 0.0;
 
     let fut1 = async {
         loop {
             app.delay_millis(1).await;
 
-            let lfo_speed = glob_lfo_speed.get().await;
-            vals = vals + lfo_speed;
-            if vals > 4095.0 {
-                vals = 0.0;
-            }
             let wave = glob_wave.get().await;
+            let lfo_speed = glob_lfo_speed.get().await;
+            let lfo_pos = glob_lfo_pos.get().await;
+            let next_pos = (lfo_pos + lfo_speed) % 4096.0;
 
-            if wave == 0 {
-                let mut lfo_pos;
-                lfo_pos = WAVEFORM_SINE[vals as usize];
-                output.set_value(lfo_pos);
-                //glob_lfo_pos.set(lfo_pos).await;
-            }
-            if wave == 1 {
-                let mut lfo_pos;
-                lfo_pos = WAVEFORM_TRIANGLE[vals as usize];
-                output.set_value(lfo_pos);
-                //glob_lfo_pos.set(lfo_pos).await;
-            }
-            if wave == 2 {
-                let mut lfo_pos;
-                lfo_pos = WAVEFORM_SAW[vals as usize];
-                output.set_value(lfo_pos);
-                //glob_lfo_pos.set(lfo_pos).await;
-            }
-            if wave == 3 {
-                let mut lfo_pos;
-                lfo_pos = WAVEFORM_RECT[vals as usize];
-                output.set_value(lfo_pos);
-                //glob_lfo_pos.set(lfo_pos).await;
-            }
+            let val = wave.at(next_pos as usize);
+
+            output.set_value(val);
+
+            let color = match wave {
+                Waveform::Sine => (50, 0, 0),
+                Waveform::Triangle => (0, 50, 0),
+                Waveform::Saw => (0, 0, 50),
+                Waveform::Rect => (50, 50, 0),
+            };
+
+            app.set_led(0, color, (val as f32 / 16.0) as u8);
+            glob_lfo_pos.set(next_pos).await;
         }
     };
 
@@ -62,10 +41,10 @@ pub async fn run(app: App<CHANNELS>) {
         let mut waiter = app.make_waiter();
         loop {
             waiter.wait_for_fader_change(0).await;
-            let mut fader = app.get_fader_values();
-            fader = [CURVE_LOG[fader[0] as usize] as u16];
-            //info!("Moved fader {} to {}", app.channels[0], fader);
-            glob_lfo_speed.set(fader[0] as f32 * 0.015 + 0.0682).await;
+            let [fader] = app.get_fader_values();
+            glob_lfo_speed
+                .set(CURVE_LOG[fader as usize] as f32 * 0.015 + 0.0682)
+                .await;
         }
     };
 
@@ -73,22 +52,10 @@ pub async fn run(app: App<CHANNELS>) {
         let mut waiter = app.make_waiter();
         loop {
             waiter.wait_for_button_down(0).await;
-            let mut wave = glob_wave.get().await;
-            wave = wave + 1;
-            if wave > 3 {
-                wave = 0;
-            }
-            glob_wave.set(wave).await;
-            info!("Wave state {}", wave);
+            let wave = glob_wave.get().await;
+            glob_wave.set(wave.cycle()).await;
         }
     };
 
-    let fut4 = async {
-        loop {
-            app.delay_millis(50).await;
-            app.set_led(0, (50, 0, 0), 50).await;
-        }
-    };
-
-    join4(fut1, fut2, fut3, fut4).await;
+    join3(fut1, fut2, fut3).await;
 }
