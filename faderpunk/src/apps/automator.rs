9ad3061327bd63aeddbd6c928a84628ed4d11a08
ArthurGibert <artgibert@gmail.com>
2025-04-03 14:04:42 +0200

    WIP

--- faderpunk/src/apps/automator.rs
+++ faderpunk/src/apps/automator.rs
@@ -1,9 +1,8 @@
 use config::{Config, Curve, Param};
 use embassy_futures::join::join3;
-use embassy_rp::trng::InverterChainLength;
 use midi2::ux::u4;
 
-use crate::{app::{App, Led, Range}, tasks::clock};
+use crate::app::{App, Led, Range};
 
 pub const CHANNELS: usize = 1;
 pub const PARAMS: usize = 2;
@@ -32,80 +31,56 @@ pub async fn run(app: App<CHANNELS>) {
     let faders = app.use_faders();
     let leds = app.use_leds();
     let midi = app.use_midi(midi_channel);
-    let mut _clock = app.use_clock();
+    let mut clock = app.use_clock();
 
-    let rec_glob = app.make_global(false);
-    leds.set(0, Led::Button, LED_COLOR, 75);
-
-  
-
+    let rec_flag = app.make_global(false);
     let jack = app.make_out_jack(0, Range::_0_10V).await;
+    
+    let mut index= 0;
+    let mut recording = false;
+    let mut buffer = [0; 16];
+
     let fut1 = async {
         loop {
-            _clock.wait_for_tick(1).await;
-            if buttons.is_button_pressed(0) {
-                rec_glob.set(true).await;
-            }  
-            else {
-                rec_glob.set(false).await;
+            clock.wait_for_tick(1).await;
+            index += 1;
+            index = index % 16;
+            
+            if index == 0 {
+                recording = false;
             }
+
+            if rec_flag.get().await  {
+                index = 0;
+                recording = true;
+            }
+
+            
+
+            if recording && buttons.is_button_pressed(0){
+                let val = faders.get_values();
+                buffer[index] = val[0];
+            }
+            
+            if !recording{
+                jack.set_value(buffer[index]);
+            }
+
         }
     };
 
     let fut2 = async {
         loop {
-            faders.wait_for_change(0).await;
-            if !rec_glob.get().await {
-
-            }
+            faders.wait_for_change(0).await;            
         }
     };
-
-let mut index = 0;
-let mut old_rec = false ;
-let mut recording = false;
-let mut lenght = 1;
-let mut start = 0;
-let mut buffer: [u16; 800] = [0; 800];
 
     let fut3 = async {
         loop {
-            if old_rec != rec_glob.get().await && !old_rec{
-                recording = !recording;
-                index = 0;
-                old_rec = true;
-            }
-
-            if old_rec != rec_glob.get().await && old_rec{
-                recording = !recording;
-                lenght = index;                
-            }
-
-
-
-        
-            app.delay_millis(10).await;
-            index += 1;
-            index  = index % 800;
-            if index == 0 {
-                recording = false;
-            }
-
-            if recording {
-                let val = faders.get_values();
-                buffer[index] = val[0];
-                jack.set_value(val[0]);
-            }
-
-            if !recording {
-                
-            }
-
-            
+            buttons.wait_for_down(0).await;
+            rec_flag.set(true).await;
         }
     };
-
-
 
     join3(fut1, fut2, fut3).await;
 }
