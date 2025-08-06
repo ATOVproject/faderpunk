94b104c8c3fdc07449cb24a19c1f1ae81766f550
ArthurGibert <artgibert@gmail.com>
2025-08-06 12:12:54 +0200

    feat(lfo): add clocked mode

--- faderpunk/src/apps/lfo.rs
+++ faderpunk/src/apps/lfo.rs
@@ -1,9 +1,12 @@
-// todo
-
-use embassy_futures::{join::join4, select::select};
+// use defmt::info;
+use embassy_futures::{
+    join::{join, join5},
+    select::select,
+};
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
+use embassy_time::Duration;
 use libfp::{
-    constants::{ATOV_BLUE, ATOV_PURPLE, ATOV_RED, ATOV_WHITE, ATOV_YELLOW, LED_MID},
+    constants::{ATOV_BLUE, ATOV_PURPLE, ATOV_RED, ATOV_WHITE, ATOV_YELLOW, LED_LOW, LED_MID},
     utils::{attenuate_bipolar, is_close, split_unsigned_value},
     Curve,
 };
@@ -12,8 +15,9 @@ use serde::{Deserialize, Serialize};
 use libfp::{Config, Waveform};
 
 use crate::{
-    app::{App, AppStorage, Led, ManagedStorage, Range, SceneEvent, RGB8},
+    app::{App, AppStorage, ClockEvent, Led, ManagedStorage, Range, SceneEvent, RGB8},
     storage::ParamStore,
+    tasks::leds::LedMode,
 };
 
 pub const CHANNELS: usize = 1;
@@ -27,6 +31,7 @@ pub struct Storage {
     fader_saved: u16,
     wave_saved: Waveform,
     att_saved: u16,
+    clocked_saved: bool,
 }
 
 impl Default for Storage {
@@ -35,6 +40,7 @@ impl Default for Storage {
             fader_saved: 2000,
             wave_saved: Waveform::Sine,
             att_saved: 4095,
+            clocked_saved: false,
         }
     }
 }
@@ -64,13 +70,18 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
     let glob_lfo_pos = app.make_global(0.0);
     let att_glob = app.make_global(4095);
     let latched_glob = app.make_global(false);
+    let clocked_glob = app.make_global(false);
+    let tick_flag = app.make_global(false);
+    let div_glob = app.make_global(24);
 
     let output = app.make_out_jack(0, Range::_Neg5_5V).await;
     let fader = app.use_faders();
     let buttons = app.use_buttons();
     let leds = app.use_leds();
+    let mut clk = app.use_clock();
 
     let curve = Curve::Logarithmic;
+    let resolution = [368, 184, 92, 48, 24, 16, 12, 8, 6];
 
     let mut shift_old = false;
 
@@ -78,6 +89,8 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
     let fader_saved = storage.query(|s| s.fader_saved).await;
     let wave_saved = storage.query(|s| s.wave_saved).await;
     let att_saved = storage.query(|s| s.att_saved).await;
+    let clocked = storage.query(|s| s.clocked_saved).await;
+    clocked_glob.set(clocked).await;
     att_glob.set(att_saved).await;
     glob_wave.set(wave_saved).await;
 
@@ -95,14 +108,38 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
         .set(curve.at(fader_saved as usize) as f32 * 0.015 + 0.0682)
         .await;
 
+    let mut count = 0;
+    let mut quant_speed: f32 = 6.;
+
     let fut1 = async {
         loop {
             app.delay_millis(1).await;
 
+            let sync = storage.query(|s| s.clocked_saved).await;
+
+            count += 1;
+            if tick_flag.get().await {
+                //add timeout
+                let div = div_glob.get().await;
+                quant_speed = 4095. / ((count * div) as f32 / 24.);
+                // info!("speed = {}, count = {}, div = {}", quant_speed, count, div);
+                count = 0;
+                tick_flag.set(false).await;
+            }
+
             let wave = glob_wave.get().await;
             let lfo_speed = glob_lfo_speed.get().await;
             let lfo_pos = glob_lfo_pos.get().await;
-            let next_pos = (lfo_pos + lfo_speed) % 4096.0;
+
+            // let next_pos = (lfo_pos + lfo_speed) % 4096.0;
+
+            let next_pos;
+            if sync {
+                next_pos = (lfo_pos + quant_speed) % 4096.0;
+            } else {
+                next_pos = (lfo_pos + lfo_speed) % 4096.0;
+            }
+
             let att = att_glob.get().await;
 
             let val = attenuate_bipolar(wave.at(next_pos as usize), att);
@@ -117,6 +154,12 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
                 Waveform::Sawinv => ATOV_RED,
                 Waveform::Rect => ATOV_WHITE,
             };
+
+            if sync && next_pos as u16 > 2048 {
+                leds.set(0, Led::Button, color, LED_LOW);
+            } else {
+                leds.set(0, Led::Button, color, LED_MID);
+            }
 
             if !buttons.is_shift_pressed() {
                 leds.set(0, Led::Top, color, led[0]);
@@ -154,6 +197,9 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
                     glob_lfo_speed
                         .set(curve.at(fader_val as usize) as f32 * 0.015 + 0.0682)
                         .await;
+                    div_glob.set(resolution[fader_val as usize / 455]).await;
+                    // info!("div = {}", div_glob.get().await);
+
                     storage
                         .modify_and_save(
                             |s| {
@@ -217,6 +263,53 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
         }
     };
 
+    let fut4 = async {
+        loop {
+            buttons
+                .wait_for_any_long_press(Duration::from_millis(500))
+                .await;
+
+            let mut wave = glob_wave.get().await;
+            for n in 0..2 {
+                wave.cycle();
+            }
+            glob_wave.set(wave).await;
+
+            let color = match wave {
+                Waveform::Sine => ATOV_YELLOW,
+                Waveform::Triangle => ATOV_PURPLE,
+                Waveform::Saw => ATOV_BLUE,
+                Waveform::Sawinv => ATOV_RED,
+                Waveform::Rect => ATOV_WHITE,
+            };
+
+            let clocked = storage
+                .modify_and_save(
+                    |s| {
+                        s.clocked_saved = !s.clocked_saved;
+                        s.clocked_saved
+                    },
+                    None,
+                )
+                .await;
+            clocked_glob.set(clocked).await;
+            if clocked {
+                leds.set_mode(0, Led::Button, LedMode::Flash(color, Some(4)));
+            }
+        }
+    };
+    let fut5 = async {
+        loop {
+            match clk.wait_for_event(24).await {
+                ClockEvent::Tick => {
+                    tick_flag.set(true).await;
+                    // info!("tick")
+                }
+                _ => {}
+            }
+        }
+    };
+
     let scene_handler = async {
         loop {
             match app.wait_for_scene_event().await {
@@ -231,6 +324,7 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
                     glob_lfo_speed
                         .set(curve.at(fader_saved as usize) as f32 * 0.015 + 0.0682)
                         .await;
+                    div_glob.set(resolution[fader_saved as usize / 455]).await;
 
                     let color = match wave_saved {
                         Waveform::Sine => ATOV_YELLOW,
@@ -247,5 +341,5 @@ pub async fn run(app: &App<CHANNELS>, _params: &Params, storage: ManagedStorage<
         }
     };
 
-    join4(fut1, fut2, fut3, scene_handler).await;
+    join(join5(fut1, fut2, fut3, fut4, scene_handler), fut5).await;
 }
