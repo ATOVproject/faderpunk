ebc759b2f4ee776ac8a3d94f8ceb40efa98292a7
Chris Maniewski <code@chmanie.com>
2025-08-18 18:05:32 +0200

    feat(calibration): add first version of automatic calibration

--- libfp/src/i2c_proto.rs
+++ libfp/src/i2c_proto.rs
@@ -7,12 +7,14 @@ use crate::types::RegressionValuesOutput;
 /// This must be large enough for the largest possible message.
 pub const MAX_MESSAGE_SIZE: usize = 192;
 
-/// WriteReadCommands sent from the calibrator to the device.
+/// WriteReadCommands sent from the i2c leader to the device
 #[derive(Serialize, Deserialize, Debug, PartialEq, Format)]
 pub enum WriteReadCommand {
+    /// Ask for the current calibration port
+    CalibPollPort,
     /// Request to set an output port to a certain value
     /// (channel, bipolar_range, value)
-    CalibSetVoltage(usize, bool, u16),
+    DacSetVoltage(usize, bool, u16),
     /// Set the calculated regression values for output voltages
     CalibSetRegressionValues(RegressionValuesOutput),
     /// Get the device's current status.
@@ -21,20 +23,26 @@ pub enum WriteReadCommand {
     SysReset,
 }
 
-/// WriteCommands sent from the calibrator to the device.
+/// WriteCommands sent from the leader to the device
 #[derive(Serialize, Deserialize, Debug, PartialEq, Format)]
 pub enum WriteCommand {
     /// Start automatic calibration
-    StartCalibration,
+    CalibStart,
+    /// Calibration: plug in this port
+    CalibPlugInPort(usize),
     /// Reset the device
     SysReset,
 }
 
-/// Responses sent from the device to the calibrator.
+/// Responses sent from the device to the leader
 #[derive(Serialize, Deserialize, Debug, PartialEq, Format)]
 pub enum Response {
-    // Tell the calibrator that we set the value
+    /// Tell the calibrator that we set the value
     CalibVoltageSet(usize),
+    /// The current calibration port
+    CalibPort(usize),
+    /// Respond with the current calibration porr
+    CalibCurrentPort(usize),
     /// The current status of the device.
     Status(DeviceStatus),
     /// Acknowledgment of a command that doesn't return data.
