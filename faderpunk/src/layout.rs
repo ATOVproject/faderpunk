ca59e7ef68199b5c70978a03df22daebaac9f7d2
Chris Maniewski <code@chmanie.com>
2025-08-27 12:29:20 +0200

    fix(layout): allow for holes in layout

--- faderpunk/src/layout.rs
+++ faderpunk/src/layout.rs
@@ -11,7 +11,7 @@ pub static LAYOUT_MANAGER: StaticCell<LayoutManager> = StaticCell::new();
 
 pub struct LayoutManager {
     exit_signals: [Signal<NoopRawMutex, bool>; GLOBAL_CHANNELS],
-    layout: Mutex<NoopRawMutex, [Option<u8>; GLOBAL_CHANNELS]>,
+    layout: Mutex<NoopRawMutex, [Option<(u8, usize)>; GLOBAL_CHANNELS]>,
     spawner: Spawner,
 }
 
@@ -25,48 +25,50 @@ impl LayoutManager {
     }
 
     async fn exit_app(&self, start_channel: usize) {
-        self.exit_signals[start_channel].signal(true);
-        Timer::after_millis(10).await;
+        let mut layout = self.layout.lock().await;
+        if layout[start_channel].is_some() {
+            layout[start_channel] = None;
+            drop(layout);
+
+            self.exit_signals[start_channel].signal(true);
+            Timer::after_millis(10).await;
+        }
     }
 
     pub async fn spawn_layout(&'static self, layout: Layout) {
+        // Build a representation of the desired layout, mapping start_channel to (app_id, channels)
+        let mut desired_layout: [Option<(u8, usize)>; GLOBAL_CHANNELS] = [None; GLOBAL_CHANNELS];
         for (app_id, start_channel, channels) in layout.iter() {
-            let current_app = {
-                let own_layout = self.layout.lock().await;
-                own_layout[start_channel]
-            };
-
-            // Only spawn if the app isn't already the one running.
-            if current_app != Some(app_id) {
-                for channel in start_channel..(start_channel + channels) {
-                    let should_exit = {
-                        let own_layout = self.layout.lock().await;
-                        own_layout[channel].is_some()
-                    };
-                    if should_exit {
-                        self.exit_app(channel).await;
-                        let mut layout = self.layout.lock().await;
-                        layout[channel] = None;
-                    }
-                }
-                // Spawn the app!
-                spawn_app_by_id(app_id, start_channel, self.spawner, &self.exit_signals).await;
-                let mut own_layout = self.layout.lock().await;
-                own_layout[start_channel] = Some(app_id);
+            if start_channel < GLOBAL_CHANNELS {
+                desired_layout[start_channel] = Some((app_id, channels));
             }
         }
 
-        // Exit any apps that are still running beyond the new layout
-        if let Some(first_free) = layout.first_free() {
-            for channel in first_free..GLOBAL_CHANNELS {
-                let should_exit = {
-                    let layout = self.layout.lock().await;
-                    layout[channel].is_some()
+        // Pass 1: Exit apps that are no longer desired or are different
+        for start_channel in 0..GLOBAL_CHANNELS {
+            let current_app = {
+                let current_layout = self.layout.lock().await;
+                current_layout[start_channel]
+            };
+
+            // If an app is running but the desired layout is different (or empty), exit the app
+            if current_app.is_some() && current_app != desired_layout[start_channel] {
+                self.exit_app(start_channel).await;
+            }
+        }
+
+        // Pass 2: Spawn new or changed apps
+        for start_channel in 0..GLOBAL_CHANNELS {
+            if let Some((app_id, channels)) = desired_layout[start_channel] {
+                let should_spawn = {
+                    let current_layout = self.layout.lock().await;
+                    current_layout[start_channel].is_none()
                 };
-                if should_exit {
-                    self.exit_app(channel).await;
-                    let mut layout = self.layout.lock().await;
-                    layout[channel] = None;
+
+                if should_spawn {
+                    spawn_app_by_id(app_id, start_channel, self.spawner, &self.exit_signals).await;
+                    let mut current_layout = self.layout.lock().await;
+                    current_layout[start_channel] = Some((app_id, channels));
                 }
             }
         }
