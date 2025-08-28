17e48d4a9f1fcf43130984e9adaa0505c5e2dae6
Chris Maniewski <code@chmanie.com>
2025-08-28 21:57:00 +0200

    feat(config): introduce more global settings, config task loop

--- faderpunk/src/tasks/global_config.rs
+++ faderpunk/src/tasks/global_config.rs
@@ -1,7 +1,10 @@
 use embassy_executor::Spawner;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Watch};
 use libfp::GlobalConfig;
+use portable_atomic::Ordering;
 
+use crate::tasks::clock::{ClockCmd, CLOCK_CMD_CHANNEL};
+use crate::tasks::leds::LED_BRIGHTNESS;
 use crate::QUANTIZER;
 
 // Receivers: ext clock loops (3), internal clock loop (1), global config loop (1)
@@ -20,6 +23,7 @@ pub async fn start_global_config(spawner: &Spawner) {
 #[embassy_executor::task]
 async fn run_global_config() {
     let mut receiver = GLOBAL_CONFIG_WATCH.receiver().unwrap();
+    let clock_sender = CLOCK_CMD_CHANNEL.sender();
     loop {
         let old = GLOBAL_CONFIG_WATCH.try_get().unwrap();
         let config = receiver.changed().await;
@@ -28,6 +32,14 @@ async fn run_global_config() {
         {
             let mut quantizer = QUANTIZER.get().lock().await;
             quantizer.set_scale(config.quantizer_key, config.quantizer_tonic);
+        }
+        if config.led_brightness != old.led_brightness {
+            LED_BRIGHTNESS.store(config.led_brightness, Ordering::Relaxed);
+        }
+        if config.internal_bpm != old.internal_bpm {
+            clock_sender
+                .send(ClockCmd::SetBpm(config.internal_bpm))
+                .await;
         }
     }
 }
