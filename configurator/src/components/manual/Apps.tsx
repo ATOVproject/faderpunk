8d0e9764102711f85be2cd3df40cc2a23ed6e1cf
Chris Maniewski <code@chmanie.com>
2025-10-14 15:21:39 +0200

    fix(manual): add app parameters and storage to manual

--- configurator/src/components/manual/Apps.tsx
+++ configurator/src/components/manual/Apps.tsx
@@ -1,5 +1,5 @@
 import { type ManualAppData, ManualApp } from "./ManualApp";
-import { H2, H3 } from "./Shared";
+import { H2 } from "./Shared";
 
 interface Props {
   apps: ManualAppData[];
@@ -8,7 +8,6 @@ interface Props {
 export const Apps = ({ apps }: Props) => (
   <>
     <H2 id="apps">Apps</H2>
-    <H3>Quickly jump to an app</H3>
     {apps.map((app) => (
       <ManualApp key={app.appId} app={app} />
     ))}
