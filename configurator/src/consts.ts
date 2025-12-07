fac17412d1c5de3a39e96a868c9d4d54458fc7d7
Chris Maniewski <code@chmanie.com>
2025-12-07 21:28:26 +0100

    feat: bump minimum version to 1.5.0

--- configurator/src/consts.ts
+++ configurator/src/consts.ts
@@ -1,2 +1,2 @@
-export const FIRMWARE_LATEST_VERSION = "1.4.0";
-export const FIRMWARE_MIN_SUPPORTED = "1.3.0";
+export const FIRMWARE_LATEST_VERSION = "1.5.0";
+export const FIRMWARE_MIN_SUPPORTED = "1.5.0";
