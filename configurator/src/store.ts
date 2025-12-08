87e3dca452617e5296701fc86dab27eac9ff1003
Chris Maniewski <code@chmanie.com>
2025-12-08 13:56:08 +0100

    fix(configurator): properly redirect for firmware update

--- configurator/src/store.ts
+++ configurator/src/store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Value, type GlobalConfig } from "@atov/fp-config";
+import semverLt from "semver/functions/lt";
 
 import type { AllApps, AppLayout, ParamValues } from "./utils/types";
 import { connectToFaderPunk, getDeviceVersion } from "./utils/usb-protocol";
@@ -9,10 +10,12 @@ import {
   getGlobalConfig,
   getLayout,
 } from "./utils/config";
+import { FIRMWARE_MIN_SUPPORTED } from "./consts";
+import { NavigateFunction } from "react-router-dom";
 
 interface State {
   apps: AllApps | undefined;
-  connect: () => Promise<void>;
+  connect: (navigate: NavigateFunction) => Promise<void>;
   config: GlobalConfig | undefined;
   disconnect: () => void;
   deviceVersion: string | undefined;
@@ -36,10 +39,20 @@ const initialState = {
 
 export const useStore = create<State>((set) => ({
   ...initialState,
-  connect: async () => {
+  connect: async (navigate: NavigateFunction) => {
     try {
       const device = await connectToFaderPunk();
       const deviceVersion = getDeviceVersion(device);
+      const updateRequired =
+        deviceVersion && semverLt(deviceVersion, FIRMWARE_MIN_SUPPORTED);
+
+      set({ deviceVersion });
+
+      if (updateRequired) {
+        navigate("/update");
+        return;
+      }
+
       const apps = await getAllApps(device);
       const params = await getAllAppParams(device);
       const layout = await getLayout(device, apps);
