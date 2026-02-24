2bd719bf01396984dab5260994a244ee0c89734d
Chris Maniewski <code@chmanie.com>
2026-02-24 22:52:23 +0200

    fix(configurator): check faderpunk version on landing page (#440)

--- configurator/src/main.tsx
+++ configurator/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { HeroUIProvider } from "@heroui/system";
-import { ToastProvider } from "@heroui/toast";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HeroUIProvider>
       <HashRouter>
-        <ToastProvider placement="top-right" />
         <App />
       </HashRouter>
     </HeroUIProvider>
